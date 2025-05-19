3abc8f95274152d61e7c07f2678406f202a288a1
Khush-hue <naglekhushant@gmail.com>
2025-05-19 11:34:49 +0530

    Day02: Loops and Factorial Programs

---
+++ Day02/loop.cpp
@@ -0,0 +1,91 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Number Printing
+    /*
+    int i;
+    for(i = 1; i<=200; i++)
+    {
+        cout<<i<<" ";
+    }
+    */
+
+    //Alphabet printing(a-z) & (A-Z)
+    /*
+    char name;
+    for (name='a'; name<='z'; name++)
+    {
+        cout<<name<<" ";
+    }
+    cout<<endl;
+    char NAME;
+    for (NAME='A'; NAME<='Z'; NAME++)
+    {
+        cout<<NAME<<" ";
+    }
+    */
+
+    //Reverse order printing
+    /*
+    int number;
+    cout<<"Enter the number:";
+    cin>>number;
+
+    for (int i = number; i > 0; i--)
+    {
+        cout<<i<<" ";
+    }
+    */
+
+    //Printing nummer in difference of 3
+    /*
+    int i;
+    for (i=1; i<=100; i=i+3)
+    {
+        cout<<i<<" ";
+    }
+    */
+
+    //table printing
+    /*
+    int number;
+    cout<<"Enter the number:";
+    cin>>number;
+
+    for (int i = 1; i<=10; i++)
+    {
+        cout<<number<<" X "<<i<<" = "<<number*i<<endl;
+    }
+
+    //Another method
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+
+    for (int i = n; i<=n*10; i=i+n)
+    {
+        cout<<i<<endl;
+    }
+    */
+
+    //Power of a number
+    /*
+    int n, pow, i, num;
+    cout<<"Enter the number: ";
+    cin>>n;
+    cout<<"Enter the power: ";
+    cin>>pow;
+
+    num = n;
+    for(i=1; i<pow; i++)
+    {
+        num = num*n;
+    }
+    cout<<num<<" ";
+    */
+
+
+
+}
